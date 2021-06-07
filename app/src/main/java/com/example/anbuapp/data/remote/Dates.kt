29532b6234b61906f60e04f6a99f6b6c95ca114b
jangkungari <jangkungarim@gmail.com>
2021-06-07 13:08:01 +0700

    first commit

---
+++ app/src/main/java/com/example/anbuapp/data/remote/Dates.kt
@@ -0,0 +1,6 @@
+package com.example.anbuapp.data.remote
+
+data class Dates(
+    val maximum: String,
+    val minimum: String
+)
---
+++ app/src/main/java/com/example/anbuapp/data/remote/Result.kt
@@ -0,0 +1,24 @@
+package com.example.anbuapp.data.remote
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Result(
+    val adult: Boolean,
+    val backdrop_path: String,
+    val genre_ids: List<Int>,
+    val id: Int,
+    val original_language: String,
+    val original_title: String,
+    val overview: String,
+    val popularity: Double,
+    val poster_path: String,
+    val release_date: String,
+    val title: String,
+    val video: Boolean,
+    val vote_average: Double,
+    val vote_count: Int
+):Parcelable{
+    val baseUrl : String get() = "https://image.tmdb.org/t/p/w500/"
+}
