7f595370e8290436c96598a74e3d0bf543f53cef
jangkungari <jangkungarim@gmail.com>
2021-06-10 13:37:27 +0700

    first commit

---
+++ app/src/main/java/com/example/anbuapp/data/remote/Movies.kt
@@ -0,0 +1,9 @@
+package com.example.anbuapp.data.remote
+
+data class Movies(
+    val dates: Dates,
+    val page: Int,
+    val results: List<Result>,
+    val total_pages: Int,
+    val total_results: Int
+)
