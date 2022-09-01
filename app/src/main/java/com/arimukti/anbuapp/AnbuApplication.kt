618dac2b776529a35862375324c5802e5289adb7
Jangkung <nodameeee@gmail.com>
2022-09-01 11:39:08 +0700

    add push notif

--- app/src/main/java/com/arimukti/anbuapp/AnbuApplication.kt
+++ app/src/main/java/com/arimukti/anbuapp/AnbuApplication.kt
@@ -1,8 +1,21 @@
 package com.arimukti.anbuapp
 
 import android.app.Application
+import com.onesignal.OneSignal
 import dagger.hilt.android.HiltAndroidApp
+
+const val ONESIGNAL_APP_ID = "9885b61b-2088-400e-99fe-618ccc101bf4"
 
 @HiltAndroidApp
 class AnbuApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Logging set to help debug issues, remove before releasing your app.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this)
+        OneSignal.setAppId(ONESIGNAL_APP_ID)
+    }
 }
--- app/src/main/java/com/arimukti/anbuapp/MainActivity.kt
+++ app/src/main/java/com/arimukti/anbuapp/MainActivity.kt
@@ -9,21 +9,30 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.arimukti.anbuapp.databinding.ActivityMainBinding
+import com.onesignal.OneSignal
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
-
+    val ONESIGNAL_APP_ID = "9885b61b-2088-400e-99fe-618ccc101bf4"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Logging set to help debug issues, remove before releasing your app.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE)
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this)
+        OneSignal.setAppId(ONESIGNAL_APP_ID)
         //View Binding
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         //Setup Navigation Controller
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         navController = navHostFragment.findNavController()
 
         val appBarConfiguration = AppBarConfiguration.Builder(
@@ -39,8 +48,6 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
-
-
 
 
 }
